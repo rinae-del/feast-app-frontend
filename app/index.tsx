c455aade8994f071e12181386d5c52525630d6ca
Katleho Mabala <katleho_m@allelectronics.co.za>
2024-08-07 10:53:22 +0200

    feat - reset password ui done

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import LoginScreen from '@/components/Screens/CustomerScreen/LoginScreen'
+import ResetPasswordScreen from '@/components/Screens/CustomerScreen/ResetPasswordScreen'
 export default function Home() {
     return (
-        <LoginScreen />
+        <ResetPasswordScreen />
     )
 }
