816d9779c420fa0af33c7240da6e7788f1948bd2
Katleho Mabala <katleho_m@allelectronics.co.za>
2024-08-08 09:37:44 +0200

    feat - business lookup user screen ui done

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import DashboardScreen from '@/components/Screens/BusinessScreen/DashboardScreen'
+import LookupUserScreen from '@/components/Screens/BusinessScreen/LookupUserScreen'
 import React from 'react'
 export default function Home() {
     return (
-        <DashboardScreen />
+        <LookupUserScreen />
     )
 }
---
+++ components/Screens/BusinessScreen/LookupUserScreen/index.tsx
@@ -0,0 +1,137 @@
+import { View, Text, TextInput, Pressable } from 'react-native'
+import React, { useState } from 'react'
+import { Colors } from '@/constants/Colors';
+import Feather from '@expo/vector-icons/Feather';
+import PhoneInput from 'react-native-international-phone-number';
+
+export default function LookupUserScreen() {
+
+    const [searchUser, setSearchUser] = useState("")
+    const [inputValue, setInputValue] = useState('');
+    const [password, setPassword] = useState("")
+    const [selectedCountry, setSelectedCountry] = useState(null);
+
+    function handleInputValue(phoneNumber: string | null | number | any) {
+        setInputValue(phoneNumber);
+    }
+
+    function handleSelectedCountry(country: string | null | number | any) {
+        setSelectedCountry(country);
+    }
+    const handleSearchUserInput = (text: string) => {
+        setSearchUser(text);
+    };
+
+
+    return (
+        <View style={{
+
+            flex: 1,
+            backgroundColor: Colors.lightGray,
+            padding: 20,
+
+        }}>
+
+            <View style={{
+                marginVertical: 10
+            }}>
+                <Text style={{
+                    color: Colors.black,
+                    fontFamily: "Montserrat_500Medium",
+                    fontSize: 16,
+                    marginBottom: 8,
+                    textAlign: 'center'
+                }}>User ID</Text>
+
+                <TextInput
+                    placeholder="XXXXXXXXX"
+                    onChangeText={handleSearchUserInput}
+                    value={searchUser}
+                    style={{
+                        backgroundColor: Colors.white,
+                        borderWidth: 1,
+                        borderColor: Colors.white,
+                        borderRadius: 10,
+                        paddingHorizontal: 10,
+                        paddingVertical: 8,
+                        fontFamily: "Montserrat_500Medium",
+                        color: "#212529"
+                    }}
+                />
+            </View>
+            <View style={{
+                marginVertical: 10
+            }}>
+                <Text style={{
+                    color: Colors.black,
+                    fontFamily: "Montserrat_500Medium",
+                    fontSize: 16,
+                    marginBottom: 8,
+                    textAlign: 'center'
+
+                }}>Or, Phone Number</Text>
+                {/* Adjusted the following to match the ui design */}
+                <PhoneInput
+                    defaultCountry="CA"
+                    placeholder="+13065014513"
+                    phoneInputStyles={{
+                        container: {
+                            backgroundColor: Colors.white,
+                            borderWidth: 1,
+                            borderStyle: 'solid',
+                            borderColor: Colors.white,
+                        },
+                        flagContainer: {
+                            backgroundColor: Colors.white,
+                            paddingHorizontal: 10,
+                            borderWidth: 0
+                        },
+                        flag: {
+                            padding: 0,
+                            borderWidth: 0
+                        },
+                        divider: {
+                            backgroundColor: Colors.white,
+                            borderWidth: 0,
+                            padding: 0,
+                            marginLeft: 0
+                        },
+                        callingCode: {
+                            margin: 0,
+                            padding: 0
+                        },
+                        input: {
+                            marginLeft: 0,
+                            fontFamily: "Montserrat_500Medium",
+                        }
+
+                    }}
+
+                    value={inputValue}
+                    onChangePhoneNumber={handleInputValue}
+                    selectedCountry={selectedCountry}
+                    onChangeSelectedCountry={handleSelectedCountry}
+                />
+            </View>
+            <Pressable style={({ pressed }) => [
+                {
+                    paddingVertical: 12,
+                    paddingHorizontal: 20,
+                    borderRadius: 100,
+                    borderWidth: 2,
+                    borderColor: Colors.black,
+
+                    fontSize: 14.4,
+                    marginVertical: 20,
+                    width: "100%",
+
+                },
+                {
+                    backgroundColor: pressed ? Colors.lightGray : Colors.black,
+                },
+            ]}>
+                <Text style={{ color: 'white', textAlign: 'center', fontSize: 15, fontFamily: 'Montserrat_800ExtraBold', }}>Lookup</Text>
+            </Pressable>
+        </View>
+    )
+}
